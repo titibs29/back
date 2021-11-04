443bbaff6e14a5c5949ea4600544c5c75ac8732a
thibaut <thibaut.rizzoli@hotmail.be>
2021-11-04 17:22:18 +0100

    fin du chapitre 1

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// CORS
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req,res,next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
 });
 
 app.use('/api/stuff', (req, res, next) => {
