82be3feba45a1dad90695ae2b68dda00434e001b
thibaut <thibaut.rizzoli@hotmail.be>
2021-11-05 18:36:52 +0100

    fin chapitre auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,15 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split (' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = 
+        const userId = decodedToken.userId;
+        if(req.body.userId && req.body.userId !== userId) {
+            throw 'invalid userId';
+        } else {
+            next()
+        };
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request !')
+        });
     }
 };
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,23 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-const stuffCtrl = require('../controllers/stuff')
-const Thing = require('../models/thing');
+const auth = require('../middleware/auth');
+const stuffCtrl = require('../controllers/stuff');
 
 
 // ajouter un truc dans la BD
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, stuffCtrl.createThing);
 
 // afficher un seul objet
-router.get('/:id', stuffCtrl.showOneThing);
+router.get('/:id', auth, stuffCtrl.showOneThing);
 
 // modifier un élément
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
 
 // supprimer un élément
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 // afficher la BD
-router.get('/', stuffCtrl.showAllThings);
+router.get('/', auth, stuffCtrl.showAllThings);
 
 module.exports = router;
