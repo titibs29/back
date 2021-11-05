edfbbe23f3e31ffc72d298a747ef5e6ea1cc5041
thibaut <thibaut.rizzoli@hotmail.be>
2021-11-05 18:15:25 +0100

    debut authentification

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -27,5 +28,6 @@ app.use((req,res,next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { typ: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true}
 });
 
